5b86c0e397485e253043c1511d20784866d4b15c
SuzanneHFarmer <36342113+SuzanneHFarmer@users.noreply.github.com>
2018-02-10 15:44:48 -0500

    Add files via upload

---
+++ Sfarmer_Week1.java
@@ -0,0 +1,25 @@
+package sfarmer_week1;
+
+/** 
+ * @Course: SDEV 250 ~ Java Programming I
+ * @Author Name: Suzanne
+ * @Assignment Name: helloworld
+ * @Date: Jan 17, 2018
+ * @Description: simple output program for formatting
+ */
+//Imports
+
+//Begin Class Sfarmer_Week1
+public class Sfarmer_Week1 {
+
+    //Begin Main Method
+    public static void main(String[] args) {
+        System.out.println("This is my Haiku");
+        System.out.println("It is a Java Haiku");
+        System.out.println("With Five Seven Five");
+        System.out.print("That means it has 5 syllables,"
+                + " then 7 syllables, then 5 syllables yet again." + "\n");
+        System.out.println("That means there are a total of 17 syllables in a Haiku.");
+    } //End Main Method
+
+} //End Class Sfarmer_Week1
